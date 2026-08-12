a0432dea9f28eab1c1c954a6ad3777d926a633bb
mogiessj <miguel.nadinic@ug.uchile.cl>
2026-08-12 03:31:48 -0400

    feat: implementacion de checkeo de colisiones, movimiento hacia atras y adelante y cambio de animacion

--- pacman.js
+++ pacman.js
@@ -5,6 +5,13 @@ class Pacman {
         this.width = width;
         this.height = height;
         this.speed = speed;
+        this.direction = DIRECTION_RIGHT;
+        this.currentFrame = 1;
+        this.frameCount = 7;
+        
+        setInterval(() => {
+            this.changeAnimation();
+        }, 100);
     }
 
     moveProcess() {
@@ -20,15 +27,47 @@ class Pacman {
     }
 
     moveBackwards() {
-
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y += this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_DOWN:
+                this.y -= this.speed;
+                break;
+        }
     }
 
     moveForwards() {
-
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y -= this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_DOWN:
+                this.y += this.speed;
+                break;
+        }
     }
 
     checkCollision() {
-
+        if(map[this.getMapY()][this.getMapX()] === 1 || 
+        map[this.getMapYRightSide()][this.getMapX()] === 1 || 
+        map[this.getMapY()][this.getMapXRightSide()] === 1 || 
+        map[this.getMapYRightSide()][this.getMapXRightSide()] === 1) {
+            return true;
+        }
+        return false;
     }
 
     checkGhostCollision() {
@@ -40,10 +79,26 @@ class Pacman {
     }
 
     changeAnimation() {
-
+        this.currentFrame = this.currentFrame === this.frameCount ? 1 : this.currentFrame + 1;
     }
 
     draw() {
 
+    }
+
+    getMapX() {
+        return parseInt(this.x/oneBlockSize);
+    }
+
+    getMapY() {
+        return parseInt(this.y/oneBlockSize);
+    }
+
+    getMapXRightSide() {
+        return parseInt((this.x + 0.9999*oneBlockSize)/oneBlockSize);
+    }
+
+    getMapYRightSide() {
+        return parseInt((this.y + 0.9999*oneBlockSize)/oneBlockSize);
     }
 }
