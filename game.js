cabc8dfc81f2a83237bc9dfc47d4ecebc981bff2
mogiessj <miguel.nadinic@ug.uchile.cl>
2026-08-14 19:22:43 -0400

    fix: corregir ID del elemento para los fotogramas de pacman

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
-const pacmanFrames = document.getElementById("animation");
+const pacmanFrames = document.getElementById("animations");
 const ghostFrames = document.getElementById("ghosts");
 
 let fps = 30;
