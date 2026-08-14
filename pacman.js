2ff16d7453b1e008692125892c116ca8169152e5
mogiessj <miguel.nadinic@ug.uchile.cl>
2026-08-14 19:18:18 -0400

    feat: metodo draw de la clase pacman

--- pacman.js
+++ pacman.js
@@ -83,7 +83,20 @@ class Pacman {
     }
 
     draw() {
-
+        canvasContext.save();
+        canvasContext.translate(this.x + oneBlockSize/2, this.y + oneBlockSize/2);
+        canvasContext.rotate((this.direction*90*Math.PI)/180);
+        canvasContext.translate(-this.x - oneBlockSize/2, -this.y - oneBlockSize/2);
+        canvasContext.drawImage(pacmanFrames, 
+            (this.currentFrame - 1)/oneBlockSize, 
+            0, 
+            oneBlockSize, 
+            oneBlockSize, 
+            this.x, 
+            this.y, 
+            this.width, 
+            this.height);
+        canvasContext.restore();
     }
 
     getMapX() {
