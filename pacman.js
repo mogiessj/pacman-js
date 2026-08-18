e9b46bc3de8d46348492ccc444bcd66d26a66aac
mogiessj <miguel.nadinic@ug.uchile.cl>
2026-08-18 01:45:32 -0400

    feat: metodo eat() implementado en la clase pacman

--- pacman.js
+++ pacman.js
@@ -24,7 +24,14 @@ class Pacman {
     }
 
     eat() {
-
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[0].length; j++) {
+                if (map[i][j] === 2 && this.getMapX() === j && this.getMapY() === i) {
+                    map[i][j] = 3;
+                    score++;
+                }
+            }
+        }
     }
 
     moveBackwards() {
